41f1ba1357cbc5724cd8bd16a12176138a193071
AnasMaimalee <anasment6@gmail.com>
2025-12-20 09:19:29 +0530

    feat: COntrollers

---
+++ app/Http/Controllers/Api/DepartmentController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(
+            Department::latest()->get()
+    );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|unique:departments,name',
+        ]);
+
+        $department = Department::create($validated);
+        return response()->json($department, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Department $department)
+    {
+        return response()->json($department);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Department $department)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|unique:departments,name',
+        ]);
+
+        $department->update($validated);
+        return response()->json($department, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Department $department)
+    {
+        $department->delete();
+        return response()->json(['message' => 'Department deleted'],);
+    }
+}
